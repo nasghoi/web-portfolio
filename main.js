9036d4f7c67ea915aebfdafd250256d0efcbc659
nasghoi <nasrullahjamsarim@gmail.com>
2026-03-28 22:10:57 +0800

    temp: enable locked

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadSkills();
     loadProjects();
-    loadContProjects(); // remove this when want to lock it again
+    // loadContProjects(); // remove this when want to lock it again
     loadEducations();
     initBackgroundGrid();
 });
